272c5fb06b90b5c640c5a936f358563712093f90
karthi283341 <karthika1592@gmail.com>
2022-10-03 15:38:10 +0530

    new code

---
+++ org/amazon/EmployeeDetails.java
@@ -0,0 +1,15 @@
+package org.amazon;
+
+public class EmployeeDetails {
+	private void username() {
+		System.out.println("enter the username");
+
+	}
+public static void main(String[] args) {
+	EmployeeDetails abc=new EmployeeDetails();
+	abc.username();
+}
+	
+	
+	
+}
